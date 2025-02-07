eb5af14357a4745607c974ecc339e49c70c5117f
Sravan Vanteru <sravan.vanteru@gmail.com>
2025-02-07 12:47:32 +0530

    event propogation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,43 +3,11 @@ import { Component } from "@angular/core";
 @Component({
     selector:'app-root',
     standalone: false,
-    template:`<h1>Hello KPMG Angular19 demo by {{name}}</h1> 
-    {{games}}
-    {{emp}}
-    <ul>
-        <li *ngFor="let game of games">{{game}}</li>
-    </ul>
-    {{emp.employeeId + " " + emp.Ename}}
-    {{emp.employeeId}} {{emp.Ename}}
-    {{emp | json}}
-<br>
-<br>
-    <table border="1">
-        <tr *ngFor="let ep of emps">
-            <td>{{ep.eid}}</td>
-            <td>{{ep.ename | uppercase}}</td>
-            <td>{{ep.ename | slice:2}}</td>
-            <td>{{ep.ename | slice:2:4}}</td>
-            <td>{{ep.gender | gender}}</td>
-            <td>{{ep.sal | currency}}</td>
-            <td>{{ep.sal | tax | currency:"INR"}}</td>
-            <td>{{ep.doj | date:"dd/MM/y"}}</td>
-            <td>
-                <img *ngIf="ep.retired" src="https://www.icegif.com/wp-content/uploads/2022/04/icegif-931.gif" height="100" width="100"/>
-                <img *ngIf="!ep.retired" src="https://media1.tenor.com/images/8f2b95f4a81cbcc482f2fd81b42baa10/tenor.gif?itemid=17302700" height="100" width="100"/>
-            </td>
-            <td> 
-                <div *ngIf="ep.retired; then thenBlock else elseBlock"></div>
-                <ng-template #thenBlock><img src="https://www.icegif.com/wp-content/uploads/2022/04/icegif-931.gif" height="100" width="100"/></ng-template>
-                <ng-template #elseBlock><img src="https://media1.tenor.com/images/8f2b95f4a81cbcc482f2fd81b42baa10/tenor.gif?itemid=17302700" height="100" width="100"/></ng-template>            
-            </td>
-        </tr>
-    </table>
-
-    <nested><nested>`
+    templateUrl:'app.component.html'
 })
 
 export class AppComponent{
+    nname:string="";
     name:string;
     games:string[] = ["Cricket","Football","Chess"]
     emp={"employeeId":101,"Ename":"Reddy"};
--- src/app/nested.component.ts
+++ src/app/nested.component.ts
@@ -1,8 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
-    selector:"nested",
+    selector: 'nested',
     standalone: false,
-    template:"<h2>nested<h2>"
+    template: `
+    <h2>Nested</h2>{{emp | json}}<button (click)="sendToParent()">send</button>
+    `
 })
-export class NestedComponent{}
+export class NestedComponent {
+    @Input() emp: any;
+
+    @Output() myevent: EventEmitter<any> = new EventEmitter();
+    sendToParent() {
+        this.myevent.emit(this.emp.ename);
+    }
+}
